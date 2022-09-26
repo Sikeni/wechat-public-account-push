e2239153ffb2eb35f55fe00e988aa57931ab8d10
Sikeni <107739975+Sikeni@users.noreply.github.com>
2022-09-26 12:33:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,12 +7,12 @@ const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: '',
+  APP_ID: 'wx4be9c11b9a491765',
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: '',
+  APP_SECRET: '601e1c3765461f2073d5dc751fcea4ee',
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -65,21 +65,21 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '老婆0',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o_6u45my-CO0Qf5VcHz_wBFgulgs',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'BtVvYT8jIEXmBT_8tCikWavMYSawLqZEwklUhgRFdSI',
       // 所在省份
-      province: '广东',
+      province: '安徽',
       // 所在城市
-      city: '肇庆',
+      city: '合肥',
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '11-24',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: 'https://wangxinleo.cn',
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
-      festivals: [
+     /* festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
           type: '*生日', name: '老婆', year: '1996', date: '09-09',
@@ -104,6 +104,7 @@ const USER_CONFIG = {
         // 退伍日
         { keyword: 'ex_day', date: '2022-09-10' }
       ],
+      */
       // 课程表相关配置
       // 如果courseSchedule不存在或者为空（null）则认为没有课程
       // 如果courseSchedule是一个数组，则认为不区分单双周，直接填写星期几对应的课表数据即可
@@ -231,7 +232,7 @@ const USER_CONFIG = {
     // 你可以不断按格式往下增加
     // ...
   ],
-
+  
   /**
    * 信息配置
    */
